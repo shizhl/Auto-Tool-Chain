719f462e6bf5b9b4659bf2af6e77dca83f867f02
ZhengliangShi <1172159897@qq.com>
2024-03-25 20:18:51 +0800

    before class Tool

--- probing/tmdb.py
+++ probing/tmdb.py
@@ -1,5 +1,5 @@
 import random
-
+from collections import defaultdict
 from model.base import Tools, TMDBTools
 from model.base import Base
 from utilize.apis import get_from_openai
@@ -7,13 +7,32 @@ from utilize.utilze import load_data, write_file
 from tqdm import tqdm
 from model.engine import *
 import ast
+import yaml
 
 model_name = 'gpt-3.5-turbo'
-headers = {
-    'Authorization': f'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGJhYjU5MGM3ZWFjYTA3ZWJlNjI1OTc0YTM3YWQ5MiIsInN1YiI6IjY1MmNmODM3NjYxMWI0MDBmZmM3MDM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.McsK4Wm5XnRSDLn62Jhy787YUAwZcQz0X5qzkGuLe_s'
-}
+dataset = 'spotify'  # spotify
+if dataset == 'tmdb':
+    headers = {
+        'Authorization': f'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGJhYjU5MGM3ZWFjYTA3ZWJlNjI1OTc0YTM3YWQ5MiIsInN1YiI6IjY1MmNmODM3NjYxMWI0MDBmZmM3MDM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.McsK4Wm5XnRSDLn62Jhy787YUAwZcQz0X5qzkGuLe_s'
+    }
+else:
+    config = yaml.load(open('/Users/shizhl/Paper2024/ProTool/dataset/spotify_config.yaml', 'r'), Loader=yaml.FullLoader)
+    os.environ['SPOTIPY_CLIENT_ID'] = config['spotipy_client_id']
+    os.environ['SPOTIPY_CLIENT_SECRET'] = config['spotipy_client_secret']
+    os.environ['SPOTIPY_REDIRECT_URI'] = config['spotipy_redirect_uri']
+    with open("/Users/shizhl/Paper2024/ProTool/specs/spotify_oas.json") as f:
+        raw_api_spec = json.load(f)
+    scopes = list(
+        raw_api_spec['components']['securitySchemes']['oauth_2_0']['flows']['authorizationCode'][
+            'scopes'].keys())
+    access_token = spotipy.util.prompt_for_user_token(scope=','.join(scopes))
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
 
-system_single = f"""In this task, you are a programming engineer. Your task is to test the provided APIs, which are provided by the TMDB platform to access the movie information.
+env = PythonExecNet(dataset=dataset)
+
+system_single = f"""In this task, you are a programming engineer. Your task is to test the provided APIs, which are provided by the OpenAPI platform to access the web information, e.g., movie and music.
 Specifically, you need to write Python code to pass the required arguments to call the APIs, getting the execution results of the APIs.
 
 You should use the following HTTP header to call the API:
@@ -84,20 +103,37 @@ Starting below, you should follow this format:
 Query: "Propose your query"
 Test code: ```python
 import json
+
 def test_code(param...):
     ...implement details
 
 # call your `test_code` function
 ...
+
 # transform the raw execution results into `str` and print the execution results
-print(json.dumps(response.json(),indent=0))
+if response.status_code == 200 and 'application/json' in response.headers.get('Content-Type', ''):
+    print(json.dumps(response.json(),indent=0))
+else:
+    print(response.text)
 ```"""
 
-tool_rank="""In this task, you are a programming engineer. Your task is to help me test one black-box API, which lacks detailed OpenAPI specification documentation. 
+tool_rank = """In this task, you are a programming engineer. Your task is to help me test one black-box API, which lacks detailed OpenAPI specification documentation. 
 Specifically, I only know the description, parameter and request url of this black-box API. However, I want to analyze its execution results. So, you need to write Python code to pass the required arguments to call the APIs, getting the execution results of the APIs.
+Here is the details of the black-box API:
 
-Since you may need the parameter to call the black-box API, I also provide your some known APIs with clear documentation, including the functional description, parameters, request type (HTTP GET or POST) and execution result example. You can use these APIs to first get required information( e.g., id), and then call the black-box API.
-"""
+{doc}
+
+Since you may need the parameter to call the black-box API, I also provide your some known APIs. You should select the APIs you need to get the required parameter (e.g., id) used call the black-box API. Here are some candidate APIs you can select:
+
+{api_list}
+
+Starting below, you should select the APIs by giving their name and write each API in a new line. And you are encouraged to select more APIs you might use.
+Your selection:
+```output:
+[1] first API name...
+[2] ...
+...
+```"""
 
 query_generation = """In this task, you are a creative software engineer and you are provide an API. Your task is to propose a specific question which can be answer by calling this API. You question will be taken as the usage and added in the API documentation.
 Here is the OpenAPI specification of the API.
@@ -108,15 +144,12 @@ Please propose one question which can be solved by calling this API:
 Question: """
 
 
-env = PythonExecNet(dataset='tmdb')
-
-toolsets = TMDBTools(
-    system='Here are some APIs used to access the TMDB platform. You need to answer the question by writing python code to call appreciate APIs and `print` the final answer. The API can be accessed via HTTP request. ',
-    oas_spec='../dataset/tmdb_api_spec.json',
-)
-
 
 def run_single_probing():
+    toolsets = TMDBTools(
+        system='Here are some APIs used to access the TMDB platform. You need to answer the question by writing python code to call appreciate APIs and `print` the final answer. The API can be accessed via HTTP request. ',
+        oas_spec='../dataset/tmdb_api_spec.json',
+    )
     # spec = load_data('../dataset/tmdb_api_spec.json')
     cnt = 0
     spec = load_data('../dataset/tmdb.pseudo_spec.json')
@@ -183,7 +216,11 @@ def normalize(sss):
     ]
     for s in tmp:
         sss = sss.replace(s, '')
-    return sss
+
+    for i in range(10):
+        sss = sss.replace(f'[{i}].', '')
+        sss = sss.replace(f'[{i}]', '')
+    return sss.strip()
 
 
 def simplify_spec(data):
@@ -200,43 +237,57 @@ def simplify_spec(data):
         for k, v in data.items():
             if k in keys_to_remove:
                 continue
-            if k == 'description':
-                results[k] = normalize(simplify_spec(v))
-            else:
-                results[k] = simplify_spec(v)
+            # if k == 'description':
+            #     results[k] = normalize(simplify_spec(v))
+            # else:
+            results[k] = simplify_spec(v)
         return results
     elif isinstance(data, list):
         return [simplify_spec(item) for item in data]
     else:
+        if type(data) == str:
+            return normalize(data)
         return data
 
 
 class Tool:
 
-    def __init__(self, tool_name, spec: dict):
-        self.name = tool_name
-        self.method, self.url = tool_name.split(' ')
+    def __init__(self, spec: dict = None):
+        if spec is None:
+            self.method, self.url, self.name = 'None', 'None', 'None'
+            self.description = 'None'
+            self.parameter = []
+            self.responses = {}
+            self.requestBody = 'None'
+            return
+
+        self.name = spec['name']
+        self.method = spec['method']
+        self.url = spec['url']
         self.description = spec['description']
         self.parameter = spec['parameters'] if 'parameters' in spec else []
         self.responses = {}
 
-        if 'responses' in spec and 'content' in spec:
+        if 'requestBody' in spec and spec['requestBody'] != None:
+            self.requestBody = simplify_spec(spec['requestBody']['content']['application/json']["schema"]['properties'])
+        else:
+            self.requestBody = 'This API do not need the request body when calling.'
+
+        if 'responses' in spec and spec['responses'] is not None and 'content' in spec['responses']:
             self.responses['responses'] = simplify_spec(spec['responses']['content']['application/json']["schema"]['properties'])
             self.responses['responses'] = json.dumps(self.responses['responses'], indent=4)
         else:
             self.responses['responses'] = 'This API has no return value.'
 
-        if '_response_json' in spec:
+        if '_response_json' in spec and spec['_response_json'] is not None:
             self.responses['_response_json'] = json.dumps(spec['_response_json'], indent=4) if type(spec['_response_json']) == dict else spec['_response_json']
         else:
             self.responses['_response_json'] = None
 
-        self.responses['_response_yaml'] = spec['_response_yaml'] if '_response_yaml' in spec else None
-
-        if 'requestBody' in spec:
-            self.requestBody = simplify_spec(spec['requestBody']['content']['application/json']["schema"]['properties'])
+        if '_response_yaml' in spec and spec['_response_yaml'] is not None:
+            self.responses['_response_yaml'] = spec['_response_yaml']
         else:
-            self.requestBody = 'This API do not need the request body when calling.'
+            self.responses['_response_yaml'] = None
 
     def update_response(self, response_format, response_example):
         if response_format == '_response_yaml':
@@ -257,11 +308,15 @@ class Tool:
                     tmp += " (type: " + p['schema']['type'] + ")"
                 parameter.append(tmp)
             parameter = '\n'.join(parameter)
+            if '{' in self.url:
+                parameter += '\nThe `{variable}` in the url path should also be replaced with actual value.'
         return parameter
 
-    def formulate(self, is_description=True, is_parameters=True, is_request_type=True,
-                  execution_results_type='responses', is_request_body=True):
-        text_doc = ["""API url: """ + self.url]
+    def formulate(self, is_description=True, is_parameters=True, is_request_type=True, is_url=True,
+                  execution_results_type=None, is_request_body=True):
+        text_doc = ["""API name: """ + self.name]
+        if is_url:
+            text_doc.append('### API url\n' + self.url)
         if is_request_type:
             method = """### Request type\n""" + self.method
             text_doc.append(method)
@@ -280,9 +335,15 @@ class Tool:
         text_doc = '\n'.join(text_doc)
         return text_doc
 
+def is_json(json_str):
+    try:
+        tmp = json.loads(json_str)
+        return True
+    except:
+        return False
+
 
 def _probing(t, tools):
-    result = {}
 
     docs = [tool.formulate(is_description=True, is_parameters=True, is_request_type=True, execution_results_type='_responses_json')
             for tool in [t] + tools]
@@ -298,27 +359,43 @@ def _probing(t, tools):
             {"role": "system", 'content': system_multi},
             {"role": "user", 'content': user_multi.format(t_doc=docs[0], docs='\n'.join([f'{i}. {e}' for i, e in enumerate(docs[1:], 1)]))},
         ]
-    code = get_from_openai(model_name=model_name, messages=message)['content']
-    pattern = r"```python(.*?)```"
-    print(code.split('\n')[0])
-    matches = re.findall(pattern, code, re.DOTALL)
-    example, state = env.run(matches[0])
+
 
     try:
-        example = json.loads(example)
+        for i in range(3):
+            code = get_from_openai(model_name=model_name, messages=message)['content']
+            pattern = r"```python(.*?)```"
+            matches = re.findall(pattern, code, re.DOTALL)
+
+            execute_code = code.split('\n')[0].replace('```python', '').replace('```', '') if len(matches) == 0 else matches[0]
+            example, state = env.run(execute_code)
+            if state == True:
+                if is_json(example):
+                    example = json.loads(example)
+                    result = {}
+                    result['example'] = example
+                    result['_responses_json'] = simplify_json(example)
+                    result['_responses_yaml'] = '\n'.join(get_yaml(example, 'execution result'))
+                    return result
+                else:
+                    message.append({"role":"assistant",'content': code})
+
+                    feedback = """Please print the raw execution results of the API in the json format as follows.
+```python
+if response.status_code == 200 and 'application/json' in response.headers.get('Content-Type', ''):
+    print(json.dumps(response.json(),indent=0))
+else:
+    print(response.text)
+```"""
+                    message.append({"role":"user",'content': feedback})
         # example = ast.literal_eval(example)
-        if state == False:
-            return None
-        else:
-            result['example'] = example
-            result['_responses_json'] = simplify_json(example)
-            result['_responses_yaml'] = '\n'.join(get_yaml(example, 'execution result'))
     except:
         return None
-    return result
+
+    return None
 
 
-def topology_probing(filename='../dataset/tmdb_api_spec.json', k=3):
+def topology_probing(filename='../dataset/tmdb_api_spec_v1.json', k=3):
     def random_sample(l, n):
         examples = []
         while len(examples) < n:
@@ -328,38 +405,69 @@ def topology_probing(filename='../dataset/tmdb_api_spec.json', k=3):
         examples = [l[i] for i in examples]
         return examples
 
+    def LLM_sample(target, candidates):
+        doc = api_spec[target].formulate(is_description=True, is_parameters=True, is_request_type=True, execution_results_type='_responses_json')
+        candidates = [
+            api_spec[e].formulate(is_description=True, is_parameters=False, is_url=False, is_request_type=False, is_request_body=False, execution_results_type=None)
+            for e in candidates
+        ]
+        candidates = '\n\n'.join([f'[{i}]. {e}' for i, e in enumerate(candidates, 1)])
+        tools = get_from_openai(model_name=model_name,
+                                messages=[{"role": "user", "content": tool_rank.format(doc=doc, api_list=candidates)}])['content']
+        tools = tools.replace('```output', '').replace('```', '')
+        tools = [normalize(e) for e in tools.split('\n') if e != '']
+        return tools
+
     cnt = 0
-    endpoints = load_data(filename)['endpoints'][:10]
-
-    api_spec, api_list = {}, []
-    for e in endpoints:
-        api_list.append(e[0])
-        api_spec[e[0]] = Tool(e[0], e[-1])
-
+    raw_spec = load_data(filename)
     cache_api = []  # 已经探索过的api
     cache_traj = []  # 探索的trajectory
-    mx = 5  # 最多轮数d
+
+    api_spec, api_list = defaultdict(Tool), []
+    for e in raw_spec['endpoints']:
+        api_list.append(e['name'])
+        api_spec[e['name']] = Tool(e)
+        if '_responses_json' in e:
+            cache_api.append(e['name'])
+
+    api_list = [e for e in api_list if e not in cache_api]
+
     # 初始
-    for api in api_list:
+    for api in tqdm(api_list):
+    # for api in tqdm(['GET_search']):
         result = _probing(api_spec[api], [])
         if result is not None:
+            print(api)
             api_spec[api].update_response('_responses_json', result['_responses_json'])
             api_spec[api].update_response('_responses_yaml', result['_responses_yaml'])
             cache_api.append(api)
 
     api_list = [e for e in api_list if e not in cache_api]
-    while len(api_list) > 0 and mx > 0:
-        mx -= 1
-        for api in api_list:
-            apis = random_sample(cache_api, k)
+    print(len(api_list))
+    while len(api_list) > 0 and cnt < 5:
+        cnt += 1
+        print(f'==========={cnt}==========')
+        for api in tqdm(api_list):
+            # apis = random_sample(cache_api, k)
+            apis = LLM_sample(api, cache_api)
+            apis = [e for e in apis if e in api_spec]
             result = _probing(api_spec[api], [api_spec[e] for e in apis])
             if result is not None:
+                print(api)
                 api_spec[api].update_response('_responses_json', result['_responses_json'])
                 api_spec[api].update_response('_responses_yaml', result['_responses_yaml'])
                 cache_api.append(api)
         api_list = [e for e in api_list if e not in cache_api]
 
-    return api_spec
+    for i, endpoint in enumerate(raw_spec['endpoints']):
+        if endpoint['name'] in cache_api:
+            raw_spec['endpoints'][i]['_responses_json'] = api_spec[endpoint['name']].responses['_responses_json']
+            raw_spec['endpoints'][i]['_responses_yaml'] = api_spec[endpoint['name']].responses['_responses_yaml']
+
+    return raw_spec
 
 
-topology_probing()
+# filename='../dataset/tmdb_api_spec_v1.json'
+filename = '../dataset/spotify.topo.v0.api_spec.json'
+res = topology_probing(filename)
+write_file(res, '../dataset/spotify.topo.v1.api_spec.json')
--- run_code.py
+++ run_code.py
@@ -1,8 +1,8 @@
-import requests
-import json
-
-headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGJhYjU5MGM3ZWFjYTA3ZWJlNjI1OTc0YTM3YWQ5MiIsInN1YiI6IjY1MmNmODM3NjYxMWI0MDBmZmM3MDM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.McsK4Wm5XnRSDLn62Jhy787YUAwZcQz0X5qzkGuLe_s'}
-
+# import requests
+# import json
+#
+# headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGJhYjU5MGM3ZWFjYTA3ZWJlNjI1OTc0YTM3YWQ5MiIsInN1YiI6IjY1MmNmODM3NjYxMWI0MDBmZmM3MDM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.McsK4Wm5XnRSDLn62Jhy787YUAwZcQz0X5qzkGuLe_s'}
+#
 code = """import requests
 import json
 
@@ -11,33 +11,45 @@ params = {}
 response = requests.get(url, headers=headers, params=params)
 print(json.dumps(response.json()))
 """
+#
+# code = '\n'.join(['\t'+e.strip() for e in code.split('\n')])
+#
+# exec_code = """import sys
+# from io import StringIO
+# import requests
+#
+# # 重定向 stdout
+# old_stdout = sys.stdout
+# redirected_output = StringIO()
+# sys.stdout = redirected_output
+#
+# headers = {{
+#     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGJhYjU5MGM3ZWFjYTA3ZWJlNjI1OTc0YTM3YWQ5MiIsInN1YiI6IjY1MmNmODM3NjYxMWI0MDBmZmM3MDM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.McsK4Wm5XnRSDLn62Jhy787YUAwZcQz0X5qzkGuLe_s'
+# }}
+#
+# try:
+# {code}
+# except Exception as e:
+#     # 捕获异常信息
+#     channel.send((str(e), False))
+# else:
+#     # 如果没有异常发生，恢复原始 stdout，并获取重定向输出的内容
+#     sys.stdout = old_stdout
+#     output = redirected_output.getvalue()
+#     # 发送标准输出内容和任何异常信息
+#     channel.send((output, True))
+# """.format(code=code)
+#
+# print(exec_code)
 
-code = '\n'.join(['\t'+e.strip() for e in code.split('\n')])
-
-exec_code = """import sys
-from io import StringIO
 import requests
+import json
 
-# 重定向 stdout
-old_stdout = sys.stdout
-redirected_output = StringIO()
-sys.stdout = redirected_output
+headers = {'Authorization': 'Bearer BQAVqRTh8fS55tDB9xseAHRmSw-kJ3odKn0KIC6H1jT5zFrhb4LVEpQ7CjhDj8n9bGBz57VQGbNE-wuAzE7C1X7Bj9erZ0CWcF5HzG3e7Y2fIUkQaZSmr0XxgwGZLXR8v576WSFJ2BwW-FYOLuJohqVuiHSjyu81WZ0PhyET8xhVyGTVvOBYAOC0ZDM1KAyXHusAKCeuGQGW4XDYjSz2FPA68tL_8JjCwM3sa-LNbyMImxe9b9YExGXSPXzaWrmxbDSGimBgN0u08Y-qze1rtOzAjF2l5aGWTmBB1XLm1VURj-H9aCejl8Ygh6gruC4UslL8dEGc0KlGBHljOsBCqnJz'}
 
-headers = {{
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGJhYjU5MGM3ZWFjYTA3ZWJlNjI1OTc0YTM3YWQ5MiIsInN1YiI6IjY1MmNmODM3NjYxMWI0MDBmZmM3MDM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.McsK4Wm5XnRSDLn62Jhy787YUAwZcQz0X5qzkGuLe_s'
-}}
+url = "https://api.spotify.com/v1/me/albums"
+params = {"ids": "4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M"} # Example Spotify IDs
 
-try:
-{code}
-except Exception as e:
-    # 捕获异常信息
-    channel.send((str(e), False))
-else:
-    # 如果没有异常发生，恢复原始 stdout，并获取重定向输出的内容
-    sys.stdout = old_stdout
-    output = redirected_output.getvalue()
-    # 发送标准输出内容和任何异常信息
-    channel.send((output, True))
-""".format(code=code)
+response = requests.delete(url, headers=headers, params=params)
 
-print(exec_code)
+print(json.dumps(response.json(), indent=0))
